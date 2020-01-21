1bd298f790d2e86ce2f78a318dddbd7661782350
tfrenzyy <tonyfrenzy@yahoo.co.uk>
2020-01-21 05:45:31 +0100

    add locations model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(App\Location::class, 20)->create();
     }
 }

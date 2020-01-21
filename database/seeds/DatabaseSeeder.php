465959d0384c489c02360fff9445011fc9f82d09
tfrenzyy <tonyfrenzy@yahoo.co.uk>
2020-01-21 06:29:55 +0100

    add Category model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
 
         factory(App\Location::class, 20)->create();
         factory(App\User::class, 10)->create();
+        factory(App\Category::class, 10)->create();
     }
 }
